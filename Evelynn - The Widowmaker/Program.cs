8cce9dba43bf532321235510e91bb483599fe0a9
SwipeDan <swipedbrain@gmail.com>
2017-01-19 21:52:23 -0200

    Added Evelynn

---
+++ Evelynn - The Widowmaker/Program.cs
@@ -0,0 +1,239 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using EloBuddy;
+using EloBuddy.SDK;
+using EloBuddy.SDK.Enumerations;
+using EloBuddy.SDK.Events;
+using EloBuddy.SDK.Menu;
+using EloBuddy.SDK.Menu.Values;
+using EloBuddy.SDK.Rendering;
+using SharpDX;
+
+
+namespace Evelynn
+{
+
+    class Program
+    {
+        private static AIHeroClient Player { get { return ObjectManager.Player; } }
+
+        private static Menu RootMenu, ComboMenu, HarassMenu, FarmingMenu, ksMenu, DrawingsMenu;
+
+        public static Spell.Active Q;
+        public static Spell.Active W;
+        public static Spell.Targeted E;
+        public static Spell.Skillshot R;
+        static void Main(string[] args)
+        {
+
+            Loading.OnLoadingComplete += Loading_OnLoadingComplete;
+
+        }
+
+        private static void Loading_OnLoadingComplete(EventArgs args)
+        {
+            Q = new Spell.Active(SpellSlot.Q, 500);
+            W = new Spell.Active(SpellSlot.W);
+            E = new Spell.Targeted(SpellSlot.E, 225);
+            R = new Spell.Skillshot(SpellSlot.R, 650, SkillShotType.Circular, 250, int.MaxValue, 350);
+            R.AllowedCollisionCount = int.MaxValue;
+
+            RootMenu = MainMenu.AddMenu("Evelynn - The Widowmaker", "Evelynn - The Widowmaker");
+
+            ComboMenu = RootMenu.AddSubMenu("Combo", "Combo");
+
+            ComboMenu.Add("UseQ", new CheckBox("Use Q"));
+            ComboMenu.Add("UseW", new CheckBox("Use W"));
+            ComboMenu.Add("UseE", new CheckBox("Use E"));
+            ComboMenu.Add("UseR", new CheckBox("Use R in combo"));
+
+            HarassMenu = RootMenu.AddSubMenu("Harass", "Harass");
+
+            HarassMenu.Add("UseQ", new CheckBox("Use Q"));
+            HarassMenu.Add("UseE", new CheckBox("Use E"));
+
+
+            FarmingMenu = RootMenu.AddSubMenu("Farming", "farming");
+
+            FarmingMenu.Add("Qclear", new CheckBox("Use Q to farm"));
+            FarmingMenu.Add("Eclear", new CheckBox("Use E to farm"));
+
+
+            DrawingsMenu = RootMenu.AddSubMenu("Drawings", "Drawings");
+
+            DrawingsMenu.Add("DrawQ", new CheckBox("Draw Q range"));
+            DrawingsMenu.Add("DrawE", new CheckBox("Draw E range"));
+            DrawingsMenu.Add("DrawRpred", new CheckBox("Draw R prediction"));
+
+            Game.OnTick += Game_OnTick;
+            Drawing.OnDraw += Drawing_OnDraw;
+
+        }
+
+        private static void Game_OnTick(EventArgs args)
+        {
+            switch (Orbwalker.ActiveModesFlags)
+            {
+                case Orbwalker.ActiveModes.Combo:
+                    Combo();
+                    break;
+                case Orbwalker.ActiveModes.Harass:
+                    Harass();
+                    break;
+                case Orbwalker.ActiveModes.LaneClear:
+                    LaneClear();
+                    break;
+                case Orbwalker.ActiveModes.JungleClear:
+                    JungleClear();
+                    break;
+                case Orbwalker.ActiveModes.LastHit:
+                    break;
+                case Orbwalker.ActiveModes.None:
+                    break;
+                default:
+                    throw new ArgumentOutOfRangeException();
+            }
+        }
+
+        private static void Drawing_OnDraw(EventArgs args)
+        {
+            var target = TargetSelector.GetTarget(Q.Range, DamageType.Physical);
+            if (DrawingsMenu["DrawQ"].Cast<CheckBox>().CurrentValue)
+            {
+                Circle.Draw(Color.Aqua, Q.Range, Player);
+            }
+
+            if (DrawingsMenu["DrawE"].Cast<CheckBox>().CurrentValue)
+            {
+                Circle.Draw(Color.Aqua, E.Range, Player);
+            }
+            if (DrawingsMenu["DrawRpred"].Cast<CheckBox>().CurrentValue)
+            {
+                if (target == null)
+                    return;
+                Drawing.DrawCircle(R.GetPrediction(target).CastPosition, 150, System.Drawing.Color.Red);
+
+            }
+        }
+
+        private static void Combo()
+        {
+            var target = TargetSelector.GetTarget(Q.Range, DamageType.Magical);
+
+            if (target == null)
+                return;
+            if (ComboMenu["UseQ"].Cast<CheckBox>().CurrentValue)
+            {
+                if (target.Distance(ObjectManager.Player) <= Q.Range && Q.IsReady())
+                {
+                    Q.Cast();
+                }
+            }
+            if (ComboMenu["UseW"].Cast<CheckBox>().CurrentValue)
+            {
+                if (target.Distance(ObjectManager.Player) <= Q.Range && W.IsReady())
+                {
+
+                    W.Cast();
+
+
+                }
+            }
+            if (ComboMenu["UseE"].Cast<CheckBox>().CurrentValue)
+            {
+                if (target.Distance(ObjectManager.Player) <= E.Range && E.IsReady())
+                {
+
+                    E.Cast(target);
+
+
+                }
+            }
+            if (ComboMenu["UseR"].Cast<CheckBox>().CurrentValue)
+            {
+                if (target.Distance(ObjectManager.Player) <= R.Range && R.IsReady()) ;
+                {
+
+                    if (R.GetPrediction(target).HitChance >= HitChance.High)
+                    {
+                        R.Cast(target);
+                    }
+
+
+                }
+            }
+
+
+        }
+        private static void Harass()
+        {
+            var target = TargetSelector.GetTarget(Q.Range, DamageType.Magical);
+
+            if (target == null)
+                return;
+            if (HarassMenu["UseQ"].Cast<CheckBox>().CurrentValue)
+            {
+                if (target.Distance(ObjectManager.Player) <= Q.Range && Q.IsReady())
+                {
+
+   
+                        Q.Cast();
+
+
+                }
+            }
+            if (HarassMenu["UseE"].Cast<CheckBox>().CurrentValue)
+            {
+                if (target.Distance(ObjectManager.Player) <= E.Range && E.IsReady())
+                {
+
+                    E.Cast(target);
+
+
+
+                }
+            }
+
+
+
+        }
+
+        private static void LaneClear()
+        {
+            if (FarmingMenu["Qclear"].Cast<CheckBox>().CurrentValue)
+            {
+                var minion1 = EntityManager.MinionsAndMonsters.EnemyMinions.FirstOrDefault(m => m.IsValidTarget(Q.Range));
+                if (minion1.Distance(ObjectManager.Player) <= Q.Range)
+                {
+                    Q.Cast();
+                }
+            }
+            if (FarmingMenu["Eclear"].Cast<CheckBox>().CurrentValue)
+            {
+                var minion1 = EntityManager.MinionsAndMonsters.EnemyMinions.FirstOrDefault(m => m.IsValidTarget(Q.Range));
+                if (minion1.Distance(ObjectManager.Player) <= E.Range)
+                {
+                    E.Cast(minion1);
+                }
+            }
+
+        }
+        private static void JungleClear()
+        {
+             foreach (var monster in EntityManager.MinionsAndMonsters.GetJungleMonsters().Where(m => m.IsMonster
+                                                                                                    && !m.IsDead))
+             {
+                 if (monster.Distance(ObjectManager.Player) <= Q.Range)
+                 {
+                     Q.Cast();
+                 }
+                 if (monster.Distance(ObjectManager.Player) <= E.Range)
+                 {
+                     E.Cast(monster);
+                 }
+             }
+
+        }
+    }
+}
